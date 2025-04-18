c5ce4b21f0adbc4bc824e40667c38082e473c661
kaif0705 <mohammedkaifkilledar5@gmail.com>
2025-04-18 12:05:46 +0530

    Work Done in Project: Implemented Custom Security

--- src/main/java/com/ecommerce/project/Model/AppRole.java
+++ src/main/java/com/ecommerce/project/Model/AppRole.java
@@ -1,7 +1,7 @@
 package com.ecommerce.project.Model;
 
 public enum AppRole {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_SELLER
+    USER,
+    ADMIN,
+    SELLER
 }
--- src/main/java/com/ecommerce/project/Model/Role.java
+++ src/main/java/com/ecommerce/project/Model/Role.java
@@ -6,8 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @NoArgsConstructor
